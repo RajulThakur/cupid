dc08e9468419b48dee0c5f34717bcd6084e8a95d
RajulThakur <139610927+RajulThakur@users.noreply.github.com>
2024-10-18 14:13:05 +0530

    feature:To type with keyboard & btn in lock page

--- app/lock/page.js
+++ app/lock/page.js
@@ -1,8 +1,45 @@
+"use client";
 import { BackspaceRounded } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 function LockPage() {
+  const [pin, setPin] = useState("");
+  const input1 = useRef(null);
+  const input2 = useRef(null);
+  const input3 = useRef(null);
+  const input4 = useRef(null);
   const styles = "px-5 py-6 text-2xl ";
+  const inputStyle =
+    "h-12 w-12 rounded-lg caret-transparent focus:outline-accent-shade-500 text-center border-2 border-slate-200 p-3 text-2xl active:border-accent-shade-300";
+  const inputRef = useMemo(
+    () => [input1, input2, input3, input4],
+    [input1, input2, input3, input4],
+  );
+  const handleClick = useCallback(
+    (e) => {
+      if (e === "Backspace" && pin.length > 0) {
+        inputRef[pin.length - 1].current.focus();
+        inputRef[pin.length - 1].current.value = "";
+        setPin((prev) => prev.slice(0, -1));
+      } else if (isFinite(e) && pin.length < 4) {
+        inputRef[pin.length].current.focus();
+        inputRef[pin.length].current.value = e;
+        setPin((prev) => prev + e);
+      }
+    },
+    [pin, inputRef]
+  );
+  useEffect(() => {
+    function handleKeyDown(e) {
+      handleClick(e.key);
+    }
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleClick]);
+  
   return (
     <div className="flex h-svh flex-col justify-between px-3">
       <section className="flex flex-col gap-10">
@@ -20,9 +57,8 @@ function LockPage() {
           {Array.from({ length: 4 }, (_, i) => (
             <input
               maxLength="1"
-              className="h-12 w-12 rounded-lg 
-              focus:outline-accent-shade-500
-              text-center border-2 border-slate-200 p-3 text-2xl active:border-accent-shade-300"
+              ref={inputRef[i]}
+              className={inputStyle}
               type="password"
               key={i}
             />
@@ -31,13 +67,21 @@ function LockPage() {
       </section>
       <section className="grid grid-cols-3">
         {Array.from({ length: 9 }, (_, i) => (
-          <button className={styles} key={`button${i + 1}`}>
+          <button
+            className={styles}
+            key={`button${i + 1}`}
+            onClick={() => handleClick(i + 1)}
+          >
             {i + 1}
           </button>
         ))}
-        <button className={styles}>.</button>
-        <button className={styles}>0</button>
-        <button className={styles}>
+        <button className={styles} onClick={() => {}}>
+          .
+        </button>
+        <button className={styles} onClick={() => handleClick(0)}>
+          0
+        </button>
+        <button className={styles} onClick={() => handleClick("Backspace")}>
           <BackspaceRounded
             sx={{ fill: "#4d6342", height: "25px", width: "25px" }}
           />
