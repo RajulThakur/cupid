e06266db5b3b7c1ad796a3d5dcf7660f98d536d8
RajulThakur <139610927+RajulThakur@users.noreply.github.com>
2024-11-05 00:23:27 +0530

    edit small

--- _actions/FindUserByEmail.js
+++ _actions/FindUserByEmail.js
@@ -1,6 +1,6 @@
 "use server";
 
-import { prisma } from "@/app/_lib/prisma";
+import prisma from "@/app/_lib/prisma";
 
 export default async function getUserByEmail(email) {
   const user = await prisma.user.findUnique({
