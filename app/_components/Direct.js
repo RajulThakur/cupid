f17caa7c031cbcc7e527a161511b6df5d8d845d0
RajulThakur <139610927+RajulThakur@users.noreply.github.com>
2024-11-09 15:06:45 +0530

    port

--- app/_components/Direct.js
+++ app/_components/Direct.js
@@ -21,7 +21,7 @@ function Direct({ data }) {
 
   useEffect(() => {
     const isBrowser = typeof window !== "undefined";
-    const wsURL = `wss://cupid-5f6c3fabade7.herokuapp.com/`;
+    const wsURL = `wss://cupid-5f6c3fabade7.herokuapp.com:8080/`;
     const newSocket = isBrowser ? new WebSocket(wsURL) : null;
     // Get the messages
     async function getMessages() {
